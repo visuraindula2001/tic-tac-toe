1f77565e583253e1c71c404cd1584aba764ab100
Visura Indula Malawara Arachchi <visuraindula2001@gmail.com>
2024-11-12 14:40:11 +0530

    Basics completed

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 
-function Square({value}){
-  function handleClick(){
-    console.log("Clicked");
+function Square() {
+  const [value,setValue] = useState(null);
+  function handleClick() {
+    setValue('X');
   }
-  return <button className="square" onClick={handleClick}>{value}</button>
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
     <>
-    <div className="board-row">
-      <Square value="1"/>
-      <Square value="2"/>
-      <Square value="3"/>
-    </div>
-    <div className="board-row">
-    <Square value="4" />
-    <Square value="5" />
-    <Square value="6" />
-    </div>
-    <div className="board-row">
-    <Square value="7"/>
-    <Square value="8" />
-    <Square value="9" />
-    </div>
+      <div className="board-row">
+        <Square  />
+        <Square  />
+        <Square  />
+      </div>
+      <div className="board-row">
+        <Square  />
+        <Square  />
+        <Square  />
+      </div>
+      <div className="board-row">
+        <Square  />
+        <Square  />
+        <Square />
+      </div>
     </>
   );
 }
-
-
-
-
-
